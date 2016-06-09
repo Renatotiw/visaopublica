067ed9e90911ede5bb96482746b78f86ebe607ad
renatodevelop <renatoalvesiw@gmail.com>
2016-06-09 16:11:26 -0300

    index

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$client = new SoapClient('http://www2.transparencia.pe.gov.br/axis/Portlet_gasto_transparencia_CredorServiceSoap?wsdl');
+
+$function = 'getTotalByCpfOrCnpj';
+
+$arguments= array(
+                        'cpfCnpj'   => '10510410000136',
+                        'name'      => 'DOCES FINOS LTDA - EPP',
+                        'mesInicial' => 1,
+                        'mesFinal'   => 12,
+                        'ano'        => 2015
+                );
+$options = array('location' => 'http://www2.transparencia.pe.gov.br/axis/Portlet_gasto_transparencia_CredorServiceSoap');
+
+$result = $client->__soapCall($function, $arguments, $options);
+
+echo 'Response: ';
+print_r($result);
+?>
